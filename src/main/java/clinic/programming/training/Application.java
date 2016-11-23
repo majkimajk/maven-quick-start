3ed7da27732baa48c2bf5ab58f5a15cc610c3aaf
majkimajk <michal.m.baran@gmail.com>
2016-11-23 21:20:25 +0100

    Changing compiler to java 8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,22 @@
 package clinic.programming.training;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+	
+	
+	public void greet() {
+		
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		
+		for (String greet : greetings) {
+			
+			System.out.println("Greetings: " + greet);
+		}
+	}
     
     public Application() {
         System.out.println ("Inside Application");
@@ -10,5 +26,7 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	
+	app.greet();
     }
 }
